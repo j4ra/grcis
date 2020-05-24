670540920b2e74ec3ed3564381d3c60788e65f5f
Jaroslav Nejedlý <jardanejedly@hotmail.com>
2020-05-24 15:48:03 +0200

    3D texture interface

---
+++ 062animation-script/Texture3D.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK;
+using Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaroslavNejedly
+{
+
+  public abstract class Texture3D : ITexture
+  {
+    public virtual Func<Intersection, Vector3d> Mapping { get; set; }
+
+    public abstract long Apply (Intersection inter);
+  }
+}
